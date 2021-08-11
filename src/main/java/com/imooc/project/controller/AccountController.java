0e7da3619b202b52b4cbb3edb2dbc99391304590
RexChen <2363613998@qq.com>
2021-08-11 22:54:10 +0800

    [Feature] 完成用户的增删改查简单版本

--- src/main/java/com/imooc/project/controller/AccountController.java
+++ src/main/java/com/imooc/project/controller/AccountController.java
@@ -1,9 +1,18 @@
 package com.imooc.project.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.imooc.project.entity.Account;
+import com.imooc.project.service.AccountService;
+import com.imooc.project.utils.ResultUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -14,7 +23,95 @@ import org.springframework.stereotype.Controller;
  * @since 2021-08-07
  */
 @Controller
-@RequestMapping("/project/account")
+@RequestMapping("/account")
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    /**
+     * 请求返回模板
+     *
+     * @return
+     */
+    @GetMapping("toList")
+    public String toList() {
+        return "account/accountList";
+    }
+
+
+    @GetMapping("list")
+    @ResponseBody
+    public R<Map<String, Object>> list(Long page, Long limit) {
+
+        Page<Account> accountPage = accountService.lambdaQuery()
+                .orderByDesc(Account::getCreateTime)
+                .page(new Page<>(page, limit));
+
+        return ResultUtil.buildPageR(accountPage);
+    }
+
+
+    /**
+     * 返回添加的模板
+     *
+     * @return
+     */
+    @GetMapping("toAdd")
+    public String toAdd() {
+        return "account/accountAdd";
+    }
+
+    /**
+     * 添加客户
+     * @param account
+     * @return
+     */
+    @PostMapping("Add")
+    @ResponseBody
+    public R<Object> add(@RequestBody Account account) {
+        return ResultUtil.buildR(accountService.save(account));
+    }
+
+
+    /**
+     * 返回添加的模板
+     *
+     * @return
+     */
+    @GetMapping("toDetail/{id}")
+    public String toDetail(@PathVariable Long id, Model model) {
+
+        Account account = accountService.getById(id);
+        model.addAttribute("account",account);
+
+        return "account/accountDetail";
+    }
+
+
+    @GetMapping("toUpdate/{id}")
+    public String toUpdate(@PathVariable Long id, Model model) {
+
+        Account account = accountService.getById(id);
+        model.addAttribute("account",account);
+
+        return "account/accountUpdate";
+    }
+
+    /**
+     * 更新用户信息
+     * @param account
+     * @return
+     */
+    @PutMapping("update")
+    public R<Object> update(@RequestBody Account account){
+        return ResultUtil.buildR(accountService.save(account));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public R<Object> delete(@PathVariable Long id){
+        return ResultUtil.buildR(accountService.removeById(id));
+    }
 
 }
---
+++ src/main/resources/static/js/account/account.js
@@ -0,0 +1,75 @@
+var table = layui.table;
+
+
+//第一个实例
+var tableIns = table.render({
+    elem: '#accountList'
+    , url: '/account/list' //数据接口
+    , page: true //开启分页
+    , parseData: function (res) { //res 即为原始返回的数据
+        return {
+            "code": res.code, //解析接口状态
+            "msg": res.msg, //解析提示文本
+            "count": res.data.count, //解析数据长度
+            "data": res.data.records //解析数据列表
+        };
+    }
+    , cols: [[ //表头
+        , {field: 'username', title: '用户名'}
+        , {field: 'realName', title: '真实姓名'}
+        , {field: 'sex', title: '性别'},
+        , {field: 'createTime', title: '创建时间'},
+        , {title: '操作', toolbar: '#barDemo'}
+    ]]
+});
+
+function toAdd() {
+    openLayer('/account/toAdd','新增用户');
+
+    layui.form.render();
+
+    mySubmit('addSubmit','POST')
+}
+
+
+
+/**
+ * 工具条事件，传入table的 lay-filter 作为标识
+ *
+ */
+//工具条事件
+table.on('tool(test)', function (obj) { //注：tool 是工具条事件名，test 是 table 原始容器的属性 lay-filter="对应的值"
+    var data = obj.data; //获得当前行数据
+    var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
+    var tr = obj.tr; //获得当前行 tr 的 DOM 对象（如果有的话）
+
+    let accountId = data.accountId;
+
+    if (layEvent === 'detail') { //查看
+        openLayer('/account/toDetail/' + accountId, '用户详情')
+
+        //渲染页面上的所有元素
+        layui.form.render();
+
+        //do somehing
+    } else if (layEvent === 'del') { //删除
+        layer.confirm('真的删除行么', function (index) {
+
+            layer.close(index);
+
+            myDelete('/account/' + accountId);
+
+        });
+    } else if (layEvent === 'edit') { //编辑
+
+        openLayer('/account/toUpdate/' + accountId, '修改用户')
+
+        //渲染页面上的所有元素
+        layui.form.render();
+
+        mySubmit('updateSubmit', 'PUT');
+
+    }
+});
+
+
